538f3d51c4cce66bd984e5d2ccfa8bfb968e7893
Shamir Duran <shamirduran15@gmail.com>
2023-03-10 14:09:09 -0500

    inicio implementacion firebase auth con google

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import { App } from './App';
+import { name as appName } from './app.json';
+import { initializeApp } from '@firebase/app';
 
-import {AppRegistry} from 'react-native';
-import {App} from './App';
-import {name as appName} from './app.json';
+const firebaseConfig = {
+    // Configuración de Firebase, puedes encontrarla en la consola de Firebase
+};
+
+// Inicializa la aplicación de Firebase
+initializeApp(firebaseConfig);
 
 AppRegistry.registerComponent(appName, () => App);
