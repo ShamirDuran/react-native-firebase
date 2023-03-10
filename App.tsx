d143d0c591da7861701239c92a6910443af35e47
Shamir Duran <shamirduran15@gmail.com>
2023-03-09 23:07:05 -0500

    init firebase auth implementation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {HomeScreen} from './src/screens/HomeScreen';
+import {SplashScreen} from './src/screens/SplashScreen';
 
 export const App = () => {
-  return <HomeScreen />;
+  return <SplashScreen />;
 };
